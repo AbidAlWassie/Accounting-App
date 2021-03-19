5f63d149ec7b9a7075cfc2e4e3fbfdeed0a02df5
Abid Al-Wasi <abidalwasi@outlook.com>
2021-03-20 00:34:31 +0600

    fixed couple of things

--- js/script.js
+++ js/script.js
@@ -44,34 +44,34 @@ document.querySelectorAll(".table-sortable th").forEach(headerCell => {
 
 
 function addRow() {
-    // get input values
-    var brand = document.getElementById("brand").value;
-    var name = document.getElementById("name").value;
-    var color = document.getElementById("color").value;
-    var size = document.getElementById("size").value;
-    var price = document.getElementById("price").value;
-    var quantity = document.getElementById("quantity").value;
-    
-    // get the html table
-    // 0 = the first table
-    var table = document.getElementsByTagName("table")[0];
-    
-    var newRow = table.insertRow(table.rows.length/2+1);
-    
-    // add cells to the row
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
-    var cell4 = newRow.insertCell(3);
-    var cell5 = newRow.insertCell(4);
-    var cell6 = newRow.insertCell(5);
-    
-    // add values to the cells
-    cell1.innerHTML = brand;
-    cell2.innerHTML = name;
-    cell3.innerHTML = color;
-    cell4.innerHTML = size;
-    cell5.innerHTML = price;
-    cell6.innerHTML = quantity;
+  // get input values
+  var brand = document.getElementById("brand").value;
+  var name = document.getElementById("name").value;
+  var color = document.getElementById("color").value;
+  var size = document.getElementById("size").value;
+  var price = document.getElementById("price").value;
+  var quantity = document.getElementById("quantity").value;
+  
+  // get the html table
+  // 0 = the first table
+  var table = document.getElementsByTagName("table")[0];
+  
+  var newRow = table.insertRow(table.rows.length/2+1);
+  
+  // add cells to the row
+  var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+  var cell3 = newRow.insertCell(2);
+  var cell4 = newRow.insertCell(3);
+  var cell5 = newRow.insertCell(4);
+  var cell6 = newRow.insertCell(5);
+  
+  // add values to the cells
+  cell1.innerHTML = brand;
+  cell2.innerHTML = name;
+  cell3.innerHTML = color;
+  cell4.innerHTML = size;
+  cell5.innerHTML = price;
+  cell6.innerHTML = quantity;
 }
 
