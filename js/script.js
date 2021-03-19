f3ffd314bea13a069047f273f4892f4e7888eaa9
Abid Al-Wasi <abidalwasi@outlook.com>
2021-03-19 21:20:01 +0600

    updated project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
   function addRow() {
     // get input values
     var brand = document.getElementById("brand").value;
+    var name = document.getElementById("name").value;
     var color = document.getElementById("color").value;
     var size = document.getElementById("size").value;
     var price = document.getElementById("price").value;
@@ -10,10 +11,6 @@
     // 0 = the first table
     var table = document.getElementsByTagName("table")[0];
     
-    // add new empty row to the table
-    // 0 = in the top 
-    // table.rows.length = the end
-    // table.rows.length/2+1 = the center
     var newRow = table.insertRow(table.rows.length/2+1);
     
     // add cells to the row
@@ -22,11 +19,13 @@
     var cell3 = newRow.insertCell(2);
     var cell4 = newRow.insertCell(3);
     var cell5 = newRow.insertCell(4);
+    var cell6 = newRow.insertCell(5);
     
     // add values to the cells
     cell1.innerHTML = brand;
-    cell2.innerHTML = color;
-    cell3.innerHTML = size;
-    cell4.innerHTML = price;
-    cell5.innerHTML = quantity;
+    cell2.innerHTML = name;
+    cell3.innerHTML = color;
+    cell4.innerHTML = size;
+    cell5.innerHTML = price;
+    cell6.innerHTML = quantity;
 }
