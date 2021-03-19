a895798ce4fc41f137c12c315d4b32b2adb8920d
Abid Al-Wasi <abidalwasi@outlook.com>
2021-03-20 00:03:47 +0600

    added sorting functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,49 @@
-  function addRow() {
+/**
+ * Sorts a HTML table.
+ *
+ * @param {HTMLTableElement} table The table to sort
+ * @param {number} column The index of the column to sort
+ * @param {boolean} asc Determines if the sorting will be in ascending
+ */
+function sortTableByColumn(table, column, asc = true) {
+  const dirModifier = asc ? 1 : -1;
+  const tBody = table.tBodies[0];
+  const rows = Array.from(tBody.querySelectorAll("tr"));
+  
+  // Sort each row
+  const sortedRows = rows.sort((a, b) => {
+    const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+    const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+    
+    return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+  });
+  
+  // Remove all existing TRs from the table
+  while (tBody.firstChild) {
+    tBody.removeChild(tBody.firstChild);
+  }
+  
+  // Re-add the newly sorted rows
+  tBody.append(...sortedRows);
+  
+  // Remember how the column is currently sorted
+  table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+  table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
+  table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
+}
+
+document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+  headerCell.addEventListener("click", () => {
+    const tableElement = headerCell.parentElement.parentElement.parentElement;
+    const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+    const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+    
+    sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+  });
+});
+
+
+function addRow() {
     // get input values
     var brand = document.getElementById("brand").value;
     var name = document.getElementById("name").value;
@@ -29,3 +74,4 @@
     cell5.innerHTML = price;
     cell6.innerHTML = quantity;
 }
+
